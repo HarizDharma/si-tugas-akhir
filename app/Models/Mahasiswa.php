8a8bf0e3571d89e1e340094a80f61a9afd5acd74
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-11 12:19:21 +0700

    MAHASIWA MODEL AND SEEDER

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'table_mahasiswa';
+    protected $fillable = [
+        'prodi',
+        'judul_skripsi',
+        'nama_dosen1',
+        'nama_dosen2',
+        'jadwal_pengambilan_ijazah',
+        'status_id',
+        'status_id',
+        'sidang_id',
+        'file_id',
+        'hasil_sidang_id'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+    public function tahapsidang()
+    {
+        return $this->belongsTo(TahapSidang::class);
+    }
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
+    public function hasilsidang()
+    {
+        return $this->belongsTo(HasilSidang::class);
+    }
+}
