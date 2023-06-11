7f743b60e70c2423730cd13fc1c7ab459cc89de4
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-11 12:16:24 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::get('user', [\App\Http\Controllers\TestEloquentController::class, 'getUser'])->name('api.user.penjaga');
+//
