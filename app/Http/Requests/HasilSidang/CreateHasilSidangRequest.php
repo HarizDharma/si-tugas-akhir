828dfa3a0062fa8bfdd2494c754c4e028e8ee6ba
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-12 18:37:11 +0700

    CREATE REQUEST

---
+++ app/Http/Requests/HasilSidang/CreateHasilSidangRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\HasilSidang;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateHasilSidangRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
