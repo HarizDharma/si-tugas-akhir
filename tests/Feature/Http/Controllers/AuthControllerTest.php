2c26f5bba43935602066e42e3be57ebc8627317e
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-13 23:24:52 +0700

    AUTH UNIT TEST

---
+++ tests/Feature/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Http\Controllers;
+
+use App\Http\Controllers\AuthController;
+use PHPUnit\Framework\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+
+    public function testLogin()
+    {
+
+    }
+
+    public function testLogout()
+    {
+
+    }
+}
