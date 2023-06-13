cc097688f11f1d1d845b86768bc11c445c1315c8
HarizDharma <jingh3191@gmail.com>
2023-06-14 04:46:08 +0700

    ADD ROUTE LOGIN AND LOGOUT

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AkademikController;
+use App\Http\Controllers\PanitiaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+//index halaman login
+Route::get('/', [AuthController::class, 'index'])->name('auth');
+
+//login action form
+Route::post('/', [AuthController::class, 'login'])->name('login');
+
+//logout action button
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+//Route::group(['middleware' => 'role:akademik'], function () {
+//    // Rute yang hanya bisa diakses oleh pengguna dengan peran "akademik"
+//    //
+//    Route::get('/dashboard', [\App\Http\Controllers\AkademikController::class, 'dashboard']);
+//});
+//
+//Route::group(['middleware' => 'role:mahasiswa'], function () {
+//    // Rute yang hanya bisa diakses oleh pengguna dengan peran "Mahasiswa"
+//
+//    Route::get('/mahasiswa', [\App\Http\Controllers\MahasiswaController::class, 'mahasiswa']);
+//});
+
+
