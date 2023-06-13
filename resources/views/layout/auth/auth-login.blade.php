fb59c417f91dfb429f9662688a12ab087aa7e602
HarizDharma <jingh3191@gmail.com>
2023-06-13 18:55:02 +0700

    LOGIN PAGE

--- resources/views/layout/auth/auth-login.blade.php
+++ resources/views/layout/auth/auth-login.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield('title')</title>
 </head>
 <body>
-<h2>PAGE LOGIN</h2>
 
 <header>
     @yield('header')
