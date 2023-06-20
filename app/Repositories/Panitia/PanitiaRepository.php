91b05f34290bc378772549d0601444dcdd770a11
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-20 17:48:11 +0700

    Updated Panitia Repository

---
+++ app/Repositories/Panitia/PanitiaRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\Panitia;
+
+use App\Http\Requests\Panitia\CreatePanitiaRequest;
+use App\Http\Requests\Panitia\UpdatePanitiaRequest;
+
+class PanitiaRepository implements PanitiaRepositoryInterface
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+
+    }
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+    public function store(CreatePanitiaRequest $request)
+    {
+        // TODO: Implement store() method.
+    }
+    public function update(UpdatePanitiaRequest $request, $id)
+    {
+        // TODO: Implement update() method.
+    }
+    public function updateSelf()
+    {
+        // TODO: Implement updateSelf() method.
+    }
+
+    public function destroy($id)
+    {
+        // TODO: Implement destroy() method.
+    }
+}
