01e84675722ecc31d8cc7d5054476e788b5422af
CallmeSetia <dwi.setia.fardhana@gmail.com>
2023-07-05 17:16:59 +0700

    Update MahasiswaRepository.php Repo : Mahasiswa

--- app/Http/Controllers/Web/WebAkademikController.php
+++ app/Http/Controllers/Web/WebAkademikController.php
@@ -2,159 +2,58 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Requests\Akademik\UpdateAkademikRequest;
+use App\Repositories\Auth\AuthRepositoryInterface;
+use App\Http\Requests\AuthRequest\LoginRequest;
 use App\Http\Controllers\Controller;
-use App\Repositories\Akademik\AkademikRepositoryInterface;
-use App\Repositories\Mahasiswa\MahasiswaRepositoryInterface;
-use App\Repositories\Panitia\PanitiaRepositoryInterface;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class WebAkademikController extends Controller
 {
-    //deklarasi untuk menmpung repository mahasiswa,panitia dan akademik
-    private $akademikRepo;
-    private $panitiaRepo;
-    private $mahasiswaRepo;
-
-    public function __construct(AkademikRepositoryInterface $akademikRepo, PanitiaRepositoryInterface $panitiaRepo, MahasiswaRepositoryInterface $mahasiswaRepo)
+    private $authRepo;
+    public function __construct(AuthRepositoryInterface $authRepo)
     {
-        //manggil repo lalu dimasukkan ke var private diatas
-        $this->akademikRepo = $akademikRepo;
-        $this->panitiaRepo = $panitiaRepo;
-        $this->mahasiswaRepo = $mahasiswaRepo;
+        $this->authRepo = $authRepo;
     }
 
     //index plus pengecekan login
     public function index()
     {
-        //get dfata return dari repo
-        // Auth berhasil
-        $user = Auth::user();
+        $auth = $this->authRepo->index();
+        if ($auth) {
+            return view('dashboard.akademik.profile')->with($auth);
+        }
+        else {
+            return view('auth');
+        }
+    }
 
-        if ($user) {
+    public function login(LoginRequest $request)
+    {
+        $auth = $this->authRepo->login($request);
+        if (!$auth) {
 
-            // Generate token JWT
-            $token = $this->generateSanctumToken($user);
-            $user->token = $token;
-
-            // Buat alert untuk berhasil login
-            session()->flash('success', 'Selamat Datang');
-
-            return view('dashboard.akademik.index')->with([
-                'token' => $token,
-                'user' => $user,
+            Alert::error('Error', 'Username atau password salah.');
+            return view('auth')->withErrors([
+                'username' => 'Username atau password salah.',
             ]);
-        } else {
-            return view('auth');
         }
+        return view('dashboard.akademik.index')->with($auth);
+
     }
-
-
-    public function dataakademik()
+    public function logout()
     {
-        //cek sudah login apa belum
-        if (Auth::check()){
-            //jika sudah login eksekusi
-            //get dfata return dari repo
-            // Auth berhasil
-            $user = Auth::user();
-
-            //dapatkan user dengan role akademik
-            $akademik = $this->akademikRepo->index();
-
-            // Generate token JWT
-            $token = $this->generateSanctumToken($user);
-            $user->token = $token;
-
-            //pengecekan login / jika login diarahkan ke hal profile
-            if ($akademik && $user) {
-                //ke halaman daftar data akademik
-                return view('dashboard.akademik.dataakademik')->with([
-                    'token' => $token,
-                    'akademik' => $akademik,
-                    'user' => $user,
-                ]);
-            }
+        $auth = $this->authRepo->logout();
+        if (!$auth) {
+            return response()->json(['message' => 'Failed to Logout'], 401);
         }
-        else {
-            // Buat alert untuk belum login
-            Alert::error('Anda Belum Login !');
-            return view('auth');
-        }
-    }
+        // Hapus sesi pengguna
+        session()->invalidate();
+        session()->regenerateToken();
 
-    public function datapanitia()
-    {
-        //cek sudah login apa belum
-        if (Auth::check()){
-            //jika sudah login eksekusi
-            //get dfata return dari repo
-            // Auth berhasil
-            $user = Auth::user();
-
-            //dapatkan user dengan role akademik
-            $akademik = $this->akademikRepo->index();
-
-            // Generate token JWT
-            $token = $this->generateSanctumToken($user);
-            $user->token = $token;
-
-            //pengecekan login / jika login diarahkan ke hal profile
-            if ($akademik && $user) {
-                //ke halaman daftar data panitia
-                return view('dashboard.akademik.datapanitia')->with([
-                    'token' => $token,
-                    'akademik' => $akademik,
-                    'user' => $user,
-                ]);
-            }
-        }
-        else {
-            // Buat alert untuk belum login
-            Alert::error('Anda Belum Login !');
-            return view('auth');
-        }
-    }
-
-    public function datamahasiswa()
-    {
-        //cek sudah login apa belum
-        if (Auth::check()){
-            //jika sudah login eksekusi
-            //get dfata return dari repo
-            // Auth berhasil
-            $user = Auth::user();
-
-            //dapatkan user dengan role akademik
-            $mahasiswa = $this->mahasiswaRepo->index();
-
-            // Generate token JWT
-            $token = $this->generateSanctumToken($user);
-            $user->token = $token;
-
-            //pengecekan login / jika login diarahkan ke hal profile
-            if ($mahasiswa && $user) {
-                //ke halaman daftar data mahasiswa
-                return view('dashboard.akademik.datamahasiswa')->with([
-                    'token' => $token,
-                    'mahasiswa' => $mahasiswa,
-                    'user' => $user,
-                ]);
-            }
-        }
-        else {
-            // Buat alert untuk belum login
-            Alert::error('Anda Belum Login !');
-            return view('auth');
-        }
-    }
-
-    protected function generateSanctumToken($user)
-    {
-        $token = $user->createToken('api-token')->plainTextToken;
-        return $token;
+        return Redirect::route('auth')->with([
+            'message' => 'Anda Berhasil Logout !',
+        ]);
     }
 
 }
