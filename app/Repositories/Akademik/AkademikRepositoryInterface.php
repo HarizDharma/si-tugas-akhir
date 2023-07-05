613f9bff57d4bd7364671cd48b0b7ac009d46bbf
CallmeSetia <dwi.setia.fardhana@gmail.com>
2023-07-05 16:50:23 +0700

    Update : READ Akademik Repository

--- app/Repositories/Akademik/AkademikRepositoryInterface.php
+++ app/Repositories/Akademik/AkademikRepositoryInterface.php
@@ -7,14 +7,14 @@ use App\Http\Requests\Akademik\UpdateAkademikRequest;
 
 interface AkademikRepositoryInterface
 {
-    public function index();
-    public function show($id);
+    public function index($platform = 'api');
+    public function show($platform = 'api', $id);
 
-    public function store(CreateAkademikRequest $request);
-    public function update(UpdateAkademikRequest $request, $id);
-    public function destroy($id);
+    public function store($platform = 'api', CreateAkademikRequest $request);
+    public function update($platform = 'api', UpdateAkademikRequest $request, $id);
+    public function destroy($platform = 'api', $id);
 
     // custom
-    public function getSelf();
-    public function updateSelf(UpdateAkademikRequest $request);
+    public function getSelf($platform = 'api' );
+    public function updateSelf($platform = 'api', UpdateAkademikRequest $request);
 }
