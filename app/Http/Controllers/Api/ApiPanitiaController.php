207f3cbca35ff45dd38ca794736ff8eca58ec466
CallmeSetia <dwi.setia.fardhana@gmail.com>
2023-07-11 09:16:15 +0700

    UPDATE PANTIA API

--- app/Http/Controllers/Api/ApiPanitiaController.php
+++ app/Http/Controllers/Api/ApiPanitiaController.php
@@ -3,9 +3,47 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Panitia\CreatePanitiaRequest;
+use App\Http\Requests\Panitia\UpdatePanitiaRequest;
+use App\Repositories\Panitia\PanitiaRepositoryInterface;
 use Illuminate\Http\Request;
 
 class ApiPanitiaController extends Controller
 {
-    //
+  private $panitiaRepo;
+    public function __construct(PanitiaRepositoryInterface $panitiaRepo)
+    {
+        $this->panitiaRepo = $panitiaRepo;
+        $this->middleware('auth:sanctum');
+    }
+
+    public function  index() {
+        $panitia = $this->panitiaRepo->index('api');
+        return response()->json($panitia);
+    }
+    public function  show($id) {
+        $panitia = $this->panitiaRepo->show('api', $id);
+        return response()->json($panitia);
+    }
+    public function  store(CreatePanitiaRequest $request) {
+        $panitia = $this->panitiaRepo->store('api', $request);
+        return response()->json($panitia);
+    }
+    public function  update(UpdatePanitiaRequest $request, $id) {
+        $panitia = $this->panitiaRepo->update('api', $request, $id);
+        return response()->json($panitia);
+    }
+    public function  destroy($id) {
+        $panitia = $this->panitiaRepo->destroy('api', $id);
+        return response()->json($panitia);
+    }
+
+
+    /// CUSTOM ---
+    ///
+
+    public function getSelf() {
+        $panitia = $this->panitiaRepo->getSelf();
+        return response()->json($panitia);
+    }
 }
