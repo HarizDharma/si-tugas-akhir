1a399204718fae1105a69e8e435b49b6b30b5565
HarizDharma <jingh3191@gmail.com>
2023-08-23 06:05:36 +0700

    Finish Modal Create Data Hasil Sidang

--- resources/views/components/modal/hasilsidangmahasiswa.blade.php
+++ resources/views/components/modal/hasilsidangmahasiswa.blade.php
@@ -11,10 +11,9 @@
             </button>
             <div class="px-6 py-6 lg:px-8">
                 <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Tambah Hasil Sidang Mahasiswa</h3>
-                <form class="space-y-6" action="{{ route('tambahHasilSidangMahasiswa', ['id' => $datamahasiswa['id']]) }}" method="POST">
+                <form class="space-y-6" action="{{ route('tambahHasilSidangMahasiswa', ['id' => $datamahasiswa['mahasiswa_id']['id']]) }}" method="POST">
                     @csrf
                     <div>
-                        <input type="hidden" name="id"value="{{ $datamahasiswa['id'] }}">
                         <label for="dosen_penguji" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Dosen penguji</label>
                         <input type="text" name="dosen_penguji" id="dosen_penguji" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                     </div>
