40390bdf956465ede103589004fe3cc91f1159da
HarizDharma <jingh3191@gmail.com>
2023-08-23 14:11:09 +0700

    Halaman ini dipindah ke dashboard panitia

--- resources/views/dashboard/akademik/datagagalsidang.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.app')
-@section('title', 'Halaman Data Gagal Sidang')
-
-@section('main')
-    <x-common.navbar :nama="$auth['data']['nama']" :role="$auth['data']['role']"/>
-    <x-sidebar.sidebarakademik/>
-    <x-table.tablegagalsidang :mahasiswa="$mahasiswa['data']"/>
-
-@endsection
