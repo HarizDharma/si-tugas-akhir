406c33e0ba7517c9585688b214560f2240699d6c
CallmeSetia <dwi.setia.fardhana@gmail.com>
2023-08-26 16:49:50 +0700

    NOTE DI FRONT END

--- resources/views/components/cardview/cardviewmahasiswa.blade.php
+++ resources/views/components/cardview/cardviewmahasiswa.blade.php
@@ -1,5 +1,8 @@
 @props(['auth'])
+{{--
 
+
+--}}
 <div class="grid grid-cols-3 gap-4 mb-4">
     <div class="bg-gray-50 dark:bg-gray-800 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Status : </h5>
--- resources/views/components/cardview/cardviewpanitia.blade.php
+++ resources/views/components/cardview/cardviewpanitia.blade.php
@@ -1,4 +1,18 @@
 @props(['data'])
+{{--    'total_mahasiswa' => $mahasiswa,--}}
+{{--    'total_panitia' => $panitia,--}}
+
+{{--    'mahasiswa_verif_panitia_sempro' => $mahasiswaSudahVerifSemproPanitia,--}}
+{{--    'mahasiswa_verif_panitia_sidang_akhir' => $mahasiswaSudahVerifSidangAkhirPanitia,--}}
+
+{{--    'mahasiswa_belum_verif_panitia_sempro' => $mahasiswaBelumVerifSemproPanitia,--}}
+{{--    'mahasiswa_belum_verif_panitia_sidang_akhir' => $mahasiswaBelumVerifSidangAkhirPanitia,--}}
+
+{{--    'mahasiswa_belum_progress' => $mahasiswaBelumProgress,--}}
+{{--    'mahasiswa_belum_sempro' =>$mahasiswaBelumSempro,--}}
+{{--    'mahasiswa_lulus_sempro' => $mahasiswaLulusSempro,--}}
+{{--    'mahasiswa_sidang_ulang' => $mahasiswaSudahSidang_Ulang,--}}
+{{--    'mahasiswa_sidang_lulus' => $mahasiswaSudahSidang_Done,--}}
 
 <div class="grid grid-cols-3 gap-4 mb-4">
     <div class="bg-gray-50 dark:bg-gray-800 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
@@ -11,13 +25,13 @@
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $data['total_panitia'] }}</h5>
     </div>
 
-    <div class="bg-gray-50 dark:bg-gray-800 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mahasiswa Lolos Daftar : </h5>
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $data['mahasiswa_belum_sempro'] }}</h5>
-    </div>
+{{--    <div class="bg-gray-50 dark:bg-gray-800 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">--}}
+{{--        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mahasiswa Lolos Daftar : </h5>--}}
+{{--        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $data['mahasiswa_belum_sempro'] }}</h5>--}}
+{{--    </div>--}}
 
-    <div class="bg-gray-50 dark:bg-gray-800 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Belum Verifikasi : </h5>
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $data['mahasiswa_belum_verif_panitia']  }}</h5>
-    </div>
+{{--    <div class="bg-gray-50 dark:bg-gray-800 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">--}}
+{{--        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Belum Verifikasi : </h5>--}}
+{{--        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $data['mahasiswa_belum_verif_panitia']  }}</h5>--}}
+{{--    </div>--}}
 </div>
